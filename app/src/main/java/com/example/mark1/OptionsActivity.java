eda37f2b350f90e01de95751196b7206d3eebe77
abhishek <abiainapur24@gmail.com>
2022-03-30 01:12:58 +0530

    SharedPrefrences added to SplashScreen Activity

--- app/src/main/java/com/example/mark1/OptionsActivity.java
+++ app/src/main/java/com/example/mark1/OptionsActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class OptionsActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
--- app/src/main/java/com/example/mark1/SplashActivity.java
+++ app/src/main/java/com/example/mark1/SplashActivity.java
@@ -3,26 +3,34 @@ package com.example.mark1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-class SplashThread extends Thread
+class SplashThread extends Thread // thread to display splash screen
 {
-    SplashActivity activity;
-    Intent intent;
+    SplashActivity activity;    // object reference of splash activity
+    Intent intent;              // intent to pass from splash to login activity or home activity
+    boolean isLoggedIn;         // user logged in or not
 
-    SplashThread(SplashActivity activity)
+    SplashThread(SplashActivity activity, boolean isLoggedIn) // constructor for thread
     {
         this.activity = activity;
+        this.isLoggedIn = isLoggedIn;
     }
 
     public void run()
     {
         try
         {
-            Thread.sleep(3000);
-            intent = new Intent(activity,LoginActivity.class);
-            activity.startActivity(intent);
-            activity.finish();
+            Thread.sleep(3000); // splash screen will appear for 3 seconds
+
+            if(isLoggedIn) // if user is already logged in, will be directed to home activity directly, else user has to log in
+                intent = new Intent(activity, HomeActivity.class);
+            else
+                intent = new Intent(activity, OptionsActivity.class);
+
+            activity.startActivity(intent); // this function actually changes activity
+            activity.finish(); // finishes current activity
         }
         catch(InterruptedException e)
         {
@@ -34,14 +42,21 @@ class SplashThread extends Thread
 
 public class SplashActivity extends AppCompatActivity
 {
+    // used to store data related to user such as name phoneNo and if user is logged in or not etc
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
-        SplashThread mySplash = new SplashThread(this);
+        getSupportActionBar().hide(); // to remove app bar for splash activity
+
+        sharedPreferences = getSharedPreferences("userInfo",MODE_PRIVATE);
+        boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn",false); // returns whether user is logged in or not
+
+        SplashThread mySplash = new SplashThread(this, isLoggedIn);
         mySplash.start();
     }
 }
