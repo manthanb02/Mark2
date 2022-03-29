9a5aa903d390509aa1a4bfe0501775d29914759e
abhishek <abiainapur24@gmail.com>
2022-03-30 03:11:02 +0530

    Login activity designed

--- app/src/main/java/com/example/mark1/OptionsActivity.java
+++ app/src/main/java/com/example/mark1/OptionsActivity.java
@@ -2,15 +2,31 @@ package com.example.mark1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OptionsActivity extends AppCompatActivity
 {
+    Button buttonRegisterBuilding; // button to register new building
+    Button buttonEnterBuilding;    // button to enter into building
+    Button buttonLogin;            // button to login
+    Intent intent;                 // to change activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_options);
+
+        buttonEnterBuilding = findViewById(R.id.buttonEnterBuilding);
+        buttonRegisterBuilding = findViewById(R.id.buttonRegisterBuilding);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(v ->
+        {
+            intent = new Intent(OptionsActivity.this,LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
